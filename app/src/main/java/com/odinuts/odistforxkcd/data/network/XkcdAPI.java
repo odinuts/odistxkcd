7b372c419c42f3a2d5d90abe309915079543b6ca
Ahmad <adawoud7@gmail.com>
2017-04-15 13:30:33 +0200

    Trying to implement an endlessly recycling recyclerview

--- app/src/main/java/com/odinuts/odistforxkcd/data/network/XkcdAPI.java
+++ app/src/main/java/com/odinuts/odistforxkcd/data/network/XkcdAPI.java
@@ -9,13 +9,13 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface XkcdAPI {
+    Retrofit retrofit = new Retrofit.Builder().baseUrl("http://xkcd.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
     @GET("http://xkcd.com/info.0.json")
     Call<XkcdResponse> getDefaultComic();
 
     @GET("http://xkcd.com/{number}/info.0.json")
     Call<XkcdResponse> getSpecificComic(@Path("number") int number);
-
-    Retrofit retrofit = new Retrofit.Builder().baseUrl("http://xkcd.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
 }
