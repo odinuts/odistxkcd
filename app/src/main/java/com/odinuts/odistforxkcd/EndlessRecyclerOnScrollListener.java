a22633b258ecf0a9728418d5306916e450cb68a0
Ahmad <adawoud7@gmail.com>
2017-04-21 16:03:00 +0200

    Implementing endless recyclerviews

--- app/src/main/java/com/odinuts/odistforxkcd/EndlessRecyclerOnScrollListener.java
+++ app/src/main/java/com/odinuts/odistforxkcd/EndlessRecyclerOnScrollListener.java
@@ -5,43 +5,41 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 
 abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
 
-    private int previousTotal = 0; // The total number of items in the data-set after the last load
-    private boolean loading = true; // True if we are still waiting for the last set of data to load.
-    private int current_page = 1;
+  private int previousTotal = 0; // The total number of items in the data-set after the last load
+  private boolean loading = true; // True if we are still waiting for the last set of data to load.
+  private int current_page = 1;
 
-    private StaggeredGridLayoutManager layoutManager;
+  private StaggeredGridLayoutManager layoutManager;
 
-    EndlessRecyclerOnScrollListener(StaggeredGridLayoutManager gridLayoutManager) {
-        layoutManager = gridLayoutManager;
+  EndlessRecyclerOnScrollListener(StaggeredGridLayoutManager gridLayoutManager) {
+    layoutManager = gridLayoutManager;
+  }
+
+  @Override public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+    super.onScrolled(recyclerView, dx, dy);
+
+    int visibleItemCount = recyclerView.getChildCount();
+    int totalItemCount = layoutManager.getItemCount();
+    int[] items = new int[3];
+    layoutManager.findFirstVisibleItemPositions(items);
+    int firstVisibleItem = items[0];
+
+    if (loading) {
+      if (totalItemCount > previousTotal) {
+        loading = false;
+        previousTotal = totalItemCount;
+      }
     }
 
-    @Override
-    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-        super.onScrolled(recyclerView, dx, dy);
-
-        int visibleItemCount = recyclerView.getChildCount();
-        int totalItemCount = layoutManager.getItemCount();
-        int[] items = new int[3];
-        layoutManager.findFirstVisibleItemPositions(items);
-        int firstVisibleItem = items[0];
-
-        if (loading) {
-            if (totalItemCount > previousTotal) {
-                loading = false;
-                previousTotal = totalItemCount;
-            }
-        }
-
-        int visibleThreshold = 4;
-        if (!loading && (totalItemCount - visibleItemCount)
-                <= (firstVisibleItem + visibleThreshold)) {
-            // End has been reached
-            // Do something
-            current_page++;
-            onLoadMore(current_page);
-            loading = true;
-        }
+    int visibleThreshold = 4;
+    if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+      // End has been reached
+      // Do something
+      current_page++;
+      onLoadMore(current_page);
+      loading = true;
     }
+  }
 
-    public abstract void onLoadMore(int current_page);
+  public abstract void onLoadMore(int current_page);
 }
--- app/src/main/java/com/odinuts/odistforxkcd/XkcdAPI.java
+++ app/src/main/java/com/odinuts/odistforxkcd/XkcdAPI.java
@@ -7,13 +7,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface XkcdAPI {
-    Retrofit retrofit = new Retrofit.Builder().baseUrl("http://xkcd.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+  Retrofit retrofit = new Retrofit.Builder().baseUrl("http://xkcd.com/")
+      .addConverterFactory(GsonConverterFactory.create())
+      .build();
 
-    @GET("http://xkcd.com/info.0.json")
-    Call<XkcdResponse> getDefaultComic();
+  @GET("http://xkcd.com/info.0.json") Call<XkcdResponse> getDefaultComic();
 
-    @GET("http://xkcd.com/{number}/info.0.json")
-    Call<XkcdResponse> getSpecificComic(@Path("number") int number);
+  @GET("http://xkcd.com/{number}/info.0.json") Call<XkcdResponse> getSpecificComic(
+      @Path("number") int number);
 }
